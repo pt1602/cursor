3b1b67089a9e0be43bd2d3121a4c7cffd4f7c2fd
Philipp Tuchardt <philipp.tuchardt@nahketing.de>
2023-02-01 07:24:29 +0100

    fix(example): dependencies

--- docs/public/libs/@pt1602/cursor/dist/js/index.js
+++ docs/public/libs/@pt1602/cursor/dist/js/index.js
@@ -1,21 +1,19 @@
 "use strict";
-const cursorDot = document.querySelector('[data-cursor-dot]');
-const cursorOutline = document.querySelector('[data-cursor-outline]');
-if (cursorDot && cursorOutline) {
-    window.addEventListener('mousemove', function (e) {
-        const posX = e.clientX;
-        const posY = e.clientY;
+window.addEventListener('mousemove', function (e) {
+    const cursorDot = document.querySelector('[data-cursor-dot]');
+    const cursorOutline = document.querySelector('[data-cursor-outline]');
+    const posX = e.clientX;
+    const posY = e.clientY;
+    if (cursorDot && cursorOutline) {
         if ("style" in cursorDot) {
             cursorDot.style.left = `${posX}px`;
         }
         if ("style" in cursorDot) {
             cursorDot.style.top = `${posY}px`;
         }
-        if ("style" in cursorOutline) {
-            cursorOutline.style.left = `${posX}px`;
-        }
-        if ("style" in cursorOutline) {
-            cursorOutline.style.top = `${posY}px`;
-        }
-    });
-}
+        cursorOutline.animate({
+            left: `${posX}px`,
+            top: `${posY}px`
+        }, { duration: 500, fill: "forwards" });
+    }
+});
